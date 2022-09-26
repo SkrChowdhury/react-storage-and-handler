1b0555af6f245b9d24ddcce97a15cc2734b3e511
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-27 02:45:29 +0600

    event handler with parameters

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import './Cosmetic.css';
 
 const Cosmetic = (props) => {
-  const {id, name, price } = props.cosmetic;
+  const { id, name, price } = props.cosmetic;
+
+  const addToCart = (id) => {
+    console.log('Item Added', id);
+  };
+
+  const addToCartWIthParam = () => addToCart(id);
   return (
-    <div>
+    <div className="product">
       <h2>Buy this: {name} </h2>
       <p>Only for ${price}</p>
-      <p><small>id: {id}</small></p>
+      <p>
+        <small>id: {id}</small>
+      </p>
+      <button onClick={addToCartWIthParam}>Add to Cart</button>
+      <button onClick={() => addToCart(id)}>Add to Cart</button> //shortcut
     </div>
   );
 };
