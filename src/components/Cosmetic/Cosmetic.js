f58c90d9768f59b012f475c85fb6dcfed767ee29
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-27 12:22:52 +0600

    localstorage in react

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { addToDb } from '../../utilities/fakedb';
 import './Cosmetic.css';
 
 const Cosmetic = (props) => {
   const { id, name, price } = props.cosmetic;
 
   const addToCart = (id) => {
-    console.log('Item Added', id);
+    addToDb(id);
   };
 
-  const addToCartWIthParam = () => addToCart(id);
+  //   const addToCartWIthParam = () => addToCart(id);
   return (
     <div className="product">
       <h2>Buy this: {name} </h2>
@@ -16,8 +17,10 @@ const Cosmetic = (props) => {
       <p>
         <small>id: {id}</small>
       </p>
-      <button onClick={addToCartWIthParam}>Add to Cart</button>
-      <button onClick={() => addToCart(id)}>Add to Cart</button> //shortcut
+      {/* <button onClick={addToCartWIthParam}>Add to Cart</button> */}
+
+      {/* shortcut */}
+      <button onClick={() => addToCart(id)}>Add to Cart</button>
     </div>
   );
 };
